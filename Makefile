d1feb6e39e7a93bd8cb13287a2ea074fa753ab39
Trafexofive <les5phenix@gmail.com>
2023-11-09 17:47:38 +0100

    commit

--- Makefile
+++ Makefile
@@ -1,2 +1,24 @@
 
+CFLAGS := -Wall -Wextra -Werror
+NAME := push_swap
+SRC := push_swap.c utils.c 
+OBJ := $(SRC:.c=.o)
+HEADER := push_swap.h
+RM := rm -rf
+CC := cc
 
+all : $(NAME)
+
+%.o : %.c 
+	$(CC) $(CFLAGS) $(SRC) -o $(NAME)
+push :
+	git add .
+	git commit -m "commit"
+	git push
+clean : $(OBJ)
+	$(RM) $(OBJ)
+fclean :
+	$(RM) $(NAME)
+re : fclean all
+
+.PHONY : clean fclean re all
--- utils.c
+++ utils.c
@@ -6,7 +6,8 @@
 // Define a structure for a node in the linked list
 
 // Function to create a new node with the given data
-Node* createNode(int data) {
+Node* createNode(int data)
+{
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = NULL;
@@ -14,23 +15,26 @@ Node* createNode(int data) {
 }
 
 // Function to insert a new node at the end of the linked list
-void insertAtEnd(Node** head, int data) {
+void insertAtEnd(Node** head, int data)
+{
     Node* newNode = createNode(data);
-    if (*head == NULL) {
+    if (*head == NULL)
         *head = newNode;
-    } else {
+	else
+	{
         Node* current = *head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
             current = current->next;
-        }
         current->next = newNode;
     }
 }
 
 // Function to traverse and print the linked list
-void traverseLinkedList(Node* head) {
+void traverseLinkedList(Node* head)
+{
     Node* current = head;
-    while (current != NULL) {
+    while (current != NULL)
+	{
         printf("%d -> ", current->data);
         current = current->next;
     }
@@ -38,30 +42,14 @@ void traverseLinkedList(Node* head) {
 }
 
 // Function to free the memory used by the linked list
-void freeLinkedList(Node* head) {
+void freeLinkedList(Node* head)
+{
     Node* current = head;
-    while (current != NULL) {
+    while (current != NULL)
+	{
         Node* temp = current;
         current = current->next;
         free(temp);
     }
 }
 
-int main() {
-    Node* head = NULL;
-
-    // Insert elements into the linked list
-    insertAtEnd(&head, 1);
-    insertAtEnd(&head, 2);
-    insertAtEnd(&head, 3);
-    insertAtEnd(&head, 4);
-
-    // Traverse and print the linked list
-    printf("Linked List: ");
-    traverseLinkedList(head);
-
-    // Free the memory used by the linked list
-    freeLinkedList(head);
-
-    return 0;
-}
